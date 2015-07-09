c76795ae3ac207cfcabb88f51cb175e0d007e2ee
stephfz <stephfz@gmail.com>
2015-07-08 21:47:01 -0500

    phantomjs and selenium demo

---
+++ codebits/demoseleniumphantomjs.py
@@ -0,0 +1,15 @@
+'''
+Install (Ubuntu)
+sudo apt-get install npm
+sudo apt-get install nodejs-legacy
+sudo npm install -g phantomjs
+'''
+
+from selenium import webdriver
+driver = webdriver.PhantomJS()
+driver.set_window_size(1120, 550)
+driver.get("https://duckduckgo.com/")
+driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+driver.find_element_by_id("search_button_homepage").click()
+print driver.current_url
+driver.quit()
